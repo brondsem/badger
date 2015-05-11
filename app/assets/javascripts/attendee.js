7634bffd5dbe0c1de5c1e640d8f171e028ce8bcf
Laura Mosher <laura.trevisan@gmail.com>
2015-05-11 06:59:31 -0400

    Fixes check in when list is filtered

--- app/assets/javascripts/attendee.js
+++ app/assets/javascripts/attendee.js
@@ -2,6 +2,7 @@ $("input[id=q_first_name_or_last_name_cont]").on("keyup", function() {
   $(this).closest("form").submit();
 });
 
-$("input[id=attendee_checked_in]").on("click", function() {
+$("input[id=attendee_checked_in]").on("change", function() {
+  console.log("checked in")
   $(this).closest("form").submit();
 });
