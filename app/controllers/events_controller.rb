7c6919400c96ba633344e1d46dce065186278390
Laura Mosher <laura.trevisan@gmail.com>
2015-09-27 20:50:04 -0400

    current user should be added to event on creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,8 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
 
+    @event.users << current_user
+
     if @event.save
       redirect_to events_path
     else
