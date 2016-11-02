f0b1d298410ea1f39438cd95d83b68c37d540b95
Laura Mosher <laura@collectiveidea.com>
2016-11-02 19:16:51 -0400

    Use store_accessors for jsonb

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -4,6 +4,18 @@ class Attendee < ActiveRecord::Base
 
   scope :alphabetical, -> { order("LOWER(last_name) asc, LOWER(first_name) asc") }
 
+  store_accessor(
+    :preferences,
+    :company,
+    :twitter,
+    :ticket_1,
+    :ticket_2,
+    :nonprofit,
+    :shirt_size,
+    :dietary_restrictions,
+    :notes
+  )
+
   def self.not_exported
     where(exported: false)
   end
